16f34aa5b36bc2eb59cfcde3e7c06d530c35b544
Kuan Jiang <sirkuanjiang@gmail.com>
2014-09-27 12:14:37 -0400

    Importing file from shell tutorial

---
+++ count.sh
@@ -0,0 +1,8 @@
+# Display the name of each data file and count of
+# the number of distict occurences of each species
+# in that file
+for filename in data/*.txt
+do
+	echo $filename
+    grep -v Species $filename | cut -d , -f 2 | sort | uniq -c
+done
